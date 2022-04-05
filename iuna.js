2071367bbb9d512c12c8d00b83a24b34c454df59
RicardoV <ricardovilov@gmail.com>
2022-04-05 00:31:03 -0400

    Gitignore funcionando

---
+++ iuna.js
@@ -0,0 +1,19 @@
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf("5176663392:AAFjFIMne8a174rxbI7Fxieupisq9doPUl4");
+
+bot.start((ctx) => {
+
+    ctx.reply("Welcome pervert")
+
+});
+
+bot.command("mywaifu",(ctx)=>{
+    ctx.reply("WaifuPic!!")
+});
+
+bot.hears(["holis iuna","HOLIS IUNA","Holis iuna","Holis Iuna"], ctx => {
+    ctx.reply("Holis "+ ctx.from.first_name)
+});
+
+bot.launch();
